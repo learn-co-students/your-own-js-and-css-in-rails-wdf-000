c45511e83cabe489aa54a329256a0139d221bd7e
Flatiron School <github email address>
2016-12-09 13:40:27 -0500

    done

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,12 +1,13 @@
 function hideWhenClicked() {
     $("#hide_this").hide()
-  
+    // or to be more specific
+    // $(".error#hide_this").hide()
 }
 
 
 // alternatively
 // function hideWhenClicked() {
-//   $(".error").on("click", function(){
+//   $(".error#hide_this").on("click", function(){
 //     $("#hide_this").hide()
 //   })
 // }
