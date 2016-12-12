59979a1d222706a9a8f296451534b069d73a4a98
jin501 <soljin01@gmail.com>
2016-12-12 01:53:36 -0500

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,10 @@
+function hideWhenClicked() {
+  // $('e').on('click', function(this) {this.hide})
+  $("#hide_this").hide()
+}
+
+// function hideWhenClicked() {
+ //   $(".error").on("click", function(){
+ //     $("#hide_this").hide()
+ //   })
+ // }
